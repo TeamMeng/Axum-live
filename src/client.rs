2b4e9a7a624db1c7c5b340a2d07c6f128304977a
TeamMeng <862915265@qq.com>
2024-10-17 16:12:04 +0800

    feature: support learn tower-http cors

--- src/client.rs
+++ src/client.rs
@@ -1,24 +1,51 @@
-use std::str::from_utf8;
+// use futures::TryStreamExt;
+// use reqwest::{Client, StatusCode};
+// use std::str::from_utf8;
 
-use futures::TryStreamExt;
-use reqwest::{Client, StatusCode};
+// #[tokio::main]
+// async fn main() {
+//     let response = Client::new()
+//         .get("http://localhost:8080/sse")
+//         .send()
+//         .await
+//         .unwrap();
+
+//     if response.status() == StatusCode::OK {
+//         let mut stream = response.bytes_stream();
+//         while let Some(chunk) = stream.try_next().await.unwrap() {
+//             let chunk_string = from_utf8(&chunk).unwrap();
+
+//             println!("{:?}", chunk_string);
+//         }
+//     } else {
+//         println!("{:?}", response.text().await.unwrap());
+//     }
+// }
+
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use tokio::net::TcpListener;
+
+async fn html() -> impl IntoResponse {
+    Html(
+        r#"
+        <script>
+            fetch('http://localhost:5000/data')
+            .then(response => response.json())
+            .then(data => console.log(data));
+        </script>
+        "#,
+    )
+}
 
 #[tokio::main]
 async fn main() {
-    let response = Client::new()
-        .get("http://localhost:8080/sse")
-        .send()
-        .await
-        .unwrap();
+    let app = Router::new().route("/", get(html));
 
-    if response.status() == StatusCode::OK {
-        let mut stream = response.bytes_stream();
-        while let Some(chunk) = stream.try_next().await.unwrap() {
-            let chunk_string = from_utf8(&chunk).unwrap();
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
 
-            println!("{:?}", chunk_string);
-        }
-    } else {
-        println!("{:?}", response.text().await.unwrap());
-    }
+    axum::serve(listener, app).await.unwrap()
 }
--- src/server.rs
+++ src/server.rs
@@ -1,36 +1,47 @@
-use axum::{
-    response::{
-        sse::{Event, KeepAlive},
-        Sse,
-    },
-    routing::get,
-    Router,
-};
-use chrono::Utc;
-use futures::{stream, Stream};
-use std::{convert::Infallible, time::Duration};
+// use axum::{
+//     http::HeaderValue,
+//     response::{
+//         sse::{KeepAlive},
+//         Sse,
+//     },
+//     routing::get,
+//     Router,
+// };
+// use chrono::Utc;
+// use futures::{stream, Stream};
+// use std::{convert::Infallible, time::Duration};
+// use tokio::net::TcpListener;
+// use tokio_stream::StreamExt;
+
+// fn get_data() -> String {
+//     format!("New data from server at : {:?}", Utc::now())
+// }
+
+// async fn sse_handler() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+//     let stream = stream::repeat_with(|| Event::default().data(get_data()))
+//         .map(Ok)
+//         .throttle(Duration::from_secs(5));
+
+//     Sse::new(stream).keep_alive(KeepAlive::new().interval(Duration::from_secs(5)))
+// }
+
+use axum::{http::HeaderValue, response::IntoResponse, routing::get, Json, Router};
 use tokio::net::TcpListener;
-use tokio_stream::StreamExt;
+use tower_http::cors::CorsLayer;
 
-fn get_data() -> String {
-    format!("New data from server at : {:?}", Utc::now())
-}
-
-async fn sse_handler() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
-    let stream = stream::repeat_with(|| Event::default().data(get_data()))
-        .map(Ok)
-        .throttle(Duration::from_secs(5));
-
-    Sse::new(stream).keep_alive(KeepAlive::new().interval(Duration::from_secs(5)))
+async fn data() -> impl IntoResponse {
+    Json("message: Hello World")
 }
 
 #[tokio::main]
 async fn main() {
-    let addr = "127.0.0.1:8080";
+    let addr = "127.0.0.1:5000";
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Server listening on {}", addr);
 
-    let app = Router::new().route("/sse", get(sse_handler));
+    let app = Router::new().route("/", get(data)).layer(
+        CorsLayer::new().allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap()),
+    );
 
     axum::serve(listener, app).await.unwrap();
 }
